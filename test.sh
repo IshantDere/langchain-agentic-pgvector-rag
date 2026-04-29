4aebab14969a2a1c71156f05f47395b4747c6ebf
IshantDere <ishantdere06@gmail.com>
2026-04-29 19:42:34 +0530

    added the env example

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 curl -X POST http://localhost:8000/chat \
 -H "Content-Type: application/json" \
 -d '{"message":"Tell me about apple laptops"}'
+
+# curl -X POST http://localhost:8000/chat \
+# -H "Content-Type: application/json" \
+# -d '{"message":"Tell me about HP laptops"}'
